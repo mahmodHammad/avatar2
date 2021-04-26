ce0d44cfe3df1d9ab765eb499ceada9b9c4577b7
= <hodaman2012@gmail.com>
2021-04-26 06:07:08 +0200

    beard with different skins

--- src/index.js
+++ src/index.js
@@ -97,35 +97,88 @@ const items ={
       Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_02.png").default,
       Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_02.png").default,
       Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_02.png").default,
+    },
+    beard:{
+      regularMustache:{
+        black:require("./low/beard/FC01_Black_Skin01.png").default,
+        blonde:require("./low/beard/FC01_Blonde_Skin01.png").default,
+        brown:require("./low/beard/FC01_Brown_Skin01.png").default,
+      },
+      curlyMustache:{
+        black:require("./low/beard/FC02_Black_Skin01.png").default,
+        blonde:require("./low/beard/FC02_Blonde_Skin01.png").default,
+        brown:require("./low/beard/FC02_Brown_Skin01.png").default,
+      },
+      fullBeard:{
+        black:require("./low/beard/FC03_Black_Skin01.png").default,
+        blonde:require("./low/beard/FC03_Blonde_Skin01.png").default,
+        brown:require("./low/beard/FC03_Brown_Skin01.png").default,
+      },     
+      shortBeard:{
+        black:require("./low/beard/FC04_Black_Skin01.png").default,
+        blonde:require("./low/beard/FC04_Blonde_Skin01.png").default,
+        brown:require("./low/beard/FC04_Brown_Skin01.png").default,
+      },    
+      Goatee:{
+        black:require("./low/beard/FC05_Black_Skin01.png").default,
+        blonde:require("./low/beard/FC05_Blonde_Skin01.png").default,
+        brown:require("./low/beard/FC05_Brown_Skin01.png").default,
+      }
     }
-
   },
-  skin3:{
-    body:require("./Avatar/BODY/Skin_03.png").default,
-    eye:require( "./Avatar/EYES/EYES_01 (Regular oval)/EYES_01_Skin_03.png").default,
-    eyeBrow:require( "./Avatar/EYEBROWS/EYEBROWS_01 (Slanted outward)/EYEBROWS_01_Skin_03.png").default,
-    ears:require( "./Avatar/EARS/EARS_01 (Regular)/Ears01_Skin03.png").default,
-    mouth:require( "./Avatar/MOUTH/MOUTH_02 (Open smile)/Mouth02_Skin03.png").default,
-    nose:require( "./Avatar/NOSE/NOSE_01 (Circular big)/NOSE_01_Skin_03.png").default,
-    glasses:{
-      Regular:require("./Avatar/GLASSES/GLASSES_01 (Regular glasses)/GLASSES_01_Skin_03.png").default,
-      Hippie:require("./Avatar/GLASSES/GLASSES_03 (Hippie)/GLASSES_03_Skin_03.png").default,
-      Sunglasses:require("./Avatar/GLASSES/GLASSES_04 (Sunglasses)/GLASSES_04_Skin_03.png").default,
-      JOY:require("./Avatar/GLASSES/GLASSES_02 (JOY glasses)/GLASSES_02_Skin_03.png").default,
-      Round: require("./Avatar/GLASSES/GLASSES_05 (Round metal frame glasses)/GLASSES_05_Skin_03.png").default,
-      patch:require("./Avatar/GLASSES/GLASSES_06 (Eye patch)/GLASSES_06_Skin_03.png").default,
-      threeD:require("./Avatar/GLASSES/GLASSES_07 (3D glasses)/GLASSES_07_Skin_03.png").default,
-      none:""
-    },hat:{
-      propeller:require("./low/HAT/HAT_01 (Propeller hat)/HAT_01_Skin_03.png").default,
-      Trucker:require("./low/HAT/HAT_02 (Trucker hat)/HAT_02_Skin_03.png").default,
-      baseball:require("./low/HAT/HAT_03 (Baseball cap)/HAT_03_Skin_03.png").default,
-      Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_03.png").default,
-      Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_03.png").default,
-      Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_03.png").default,
-    }
+  // skin3:{
+  //   body:require("./Avatar/BODY/Skin_03.png").default,
+  //   eye:require( "./Avatar/EYES/EYES_01 (Regular oval)/EYES_01_Skin_03.png").default,
+  //   eyeBrow:require( "./Avatar/EYEBROWS/EYEBROWS_01 (Slanted outward)/EYEBROWS_01_Skin_03.png").default,
+  //   ears:require( "./Avatar/EARS/EARS_01 (Regular)/Ears01_Skin03.png").default,
+  //   mouth:require( "./Avatar/MOUTH/MOUTH_02 (Open smile)/Mouth02_Skin03.png").default,
+  //   nose:require( "./Avatar/NOSE/NOSE_01 (Circular big)/NOSE_01_Skin_03.png").default,
+  //   glasses:{
+  //     Regular:require("./Avatar/GLASSES/GLASSES_01 (Regular glasses)/GLASSES_01_Skin_03.png").default,
+  //     Hippie:require("./Avatar/GLASSES/GLASSES_03 (Hippie)/GLASSES_03_Skin_03.png").default,
+  //     Sunglasses:require("./Avatar/GLASSES/GLASSES_04 (Sunglasses)/GLASSES_04_Skin_03.png").default,
+  //     JOY:require("./Avatar/GLASSES/GLASSES_02 (JOY glasses)/GLASSES_02_Skin_03.png").default,
+  //     Round: require("./Avatar/GLASSES/GLASSES_05 (Round metal frame glasses)/GLASSES_05_Skin_03.png").default,
+  //     patch:require("./Avatar/GLASSES/GLASSES_06 (Eye patch)/GLASSES_06_Skin_03.png").default,
+  //     threeD:require("./Avatar/GLASSES/GLASSES_07 (3D glasses)/GLASSES_07_Skin_03.png").default,
+  //     none:""
+  //   },hat:{
+  //     propeller:require("./low/HAT/HAT_01 (Propeller hat)/HAT_01_Skin_03.png").default,
+  //     Trucker:require("./low/HAT/HAT_02 (Trucker hat)/HAT_02_Skin_03.png").default,
+  //     baseball:require("./low/HAT/HAT_03 (Baseball cap)/HAT_03_Skin_03.png").default,
+  //     Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_03.png").default,
+  //     Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_03.png").default,
+  //     Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_03.png").default,
+  //   },
+  //   beard:{
+  //     regularMustache:{
+  //       black:require("./low/beard/FC01_Black_Skin01.png").default,
+  //       blonde:require("./low/beard/FC01_Blonde_Skin01.png").default,
+  //       brown:require("./low/beard/FC01_Brown_Skin01.png").default,
+  //     },
+  //     curlyMustache:{
+  //       black:require("./low/beard/FC02_Black_Skin01.png").default,
+  //       blonde:require("./low/beard/FC02_Blonde_Skin01.png").default,
+  //       brown:require("./low/beard/FC02_Brown_Skin01.png").default,
+  //     },
+  //     fullBeard:{
+  //       black:require("./low/beard/FC03_Black_Skin01.png").default,
+  //       blonde:require("./low/beard/FC03_Blonde_Skin01.png").default,
+  //       brown:require("./low/beard/FC03_Brown_Skin01.png").default,
+  //     },     
+  //     shortBeard:{
+  //       black:require("./low/beard/FC04_Black_Skin01.png").default,
+  //       blonde:require("./low/beard/FC04_Blonde_Skin01.png").default,
+  //       brown:require("./low/beard/FC04_Brown_Skin01.png").default,
+  //     },    
+  //     Goatee:{
+  //       black:require("./low/beard/FC05_Black_Skin01.png").default,
+  //       blonde:require("./low/beard/FC05_Blonde_Skin01.png").default,
+  //       brown:require("./low/beard/FC05_Brown_Skin01.png").default,
+  //     }
+  //   }
 
-  },
+  // },
 }
 const undependentItems={
   clothes:{
