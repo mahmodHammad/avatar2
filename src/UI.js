41625589698a7491df80ae99724c88db8480b0f1
= <hodaman2012@gmail.com>
2021-04-30 03:43:00 +0200

    refactor UI control creating

--- src/UI.js
+++ src/UI.js
@@ -12,19 +12,22 @@ const hairColors={
   cyan:0x3264c8
 }
 
-function UI({handleExport,glasses,changeGlasses,
-  changeSkin ,skins,clothes, changeCloth,hats,changeHat,
-  beard , changeBeard
-}) {
+function UI({handleExport,controllers}) {
 
   return (
       <React.Fragment>
         <div className="l-panel panel">
-             <AvatarChange title="Skintone" items={skins} changeItem={changeSkin}/>
+          {controllers.map(ctrl=><React.Fragment>
+            {console.log("FJFJF",ctrl[0])}
+            {console.log("aaaFJFJF",ctrl[1])}
+            <AvatarChange title="Skintone" items={ctrl[0]} changeItem={ctrl[1]}/>
+          </React.Fragment>)}
+             {/* <AvatarChange title="Skintone" items={skins} changeItem={changeSkin}/>
              <AvatarChange title="Glasses" items={glasses} changeItem={changeGlasses}/>
              <AvatarChange title="Clothes" items={clothes} changeItem={changeCloth}/>
              <AvatarChange title="Hats" items={hats} changeItem={changeHat}/>
              <AvatarChange title="Beard" items={beard} changeItem={changeBeard}/>
+             <AvatarChange title="eyebrow" items={eyebrow} changeItem={changeeyebrow}/> */}
         </div>
          <div className="r-panel panel">
            {/* <AvatarChangeColor title="Face Color"   colors={faceColors}/> */}
--- src/index.js
+++ src/index.js
@@ -5,181 +5,7 @@ import { render } from 'react-dom';
 import { Stage, Layer, Image } from 'react-konva';
 import useImage from 'use-image';
 import UI from "./UI"
-
-const items ={
-  skin1:{
-    body:require("./low/BODY/Skin_01.png").default,
-    eye:require( "./low/EYES/EYES_01 (Regular oval)/EYES_01_Skin_01.png").default,
-    eyeBrow:require( "./low/EYEBROWS/EYEBROWS_01 (Slanted outward)/EYEBROWS_01_Skin_01.png").default,
-    ears:require( "./low/EARS/EARS_01 (Regular)/Ears01_Skin01.png").default,
-    mouth:require( "./low/MOUTH/MOUTH_02 (Open smile)/Mouth02_Skin01.png").default,
-    nose:require( "./low/NOSE/NOSE_01 (Circular big)/NOSE_01_Skin_01.png").default,
-    glasses:{
-      Regular:require("./low/GLASSES/GLASSES_01 (Regular glasses)/GLASSES_01_Skin_01.png").default,
-      Hippie:require("./low/GLASSES/GLASSES_03 (Hippie)/GLASSES_03_Skin_01.png").default,
-      Sunglasses:require("./low/GLASSES/GLASSES_04 (Sunglasses)/GLASSES_04_Skin_01.png").default,
-      JOY:require("./low/GLASSES/GLASSES_02 (JOY glasses)/GLASSES_02_Skin_01.png").default,
-      Round: require("./low/GLASSES/GLASSES_05 (Round metal frame glasses)/GLASSES_05_Skin_01.png").default,
-      patch:require("./low/GLASSES/GLASSES_06 (Eye patch)/GLASSES_06_Skin_01.png").default,
-      threeD:require("./low/GLASSES/GLASSES_07 (3D glasses)/GLASSES_07_Skin_01.png").default,
-      none:""
-    },
-    hat:{
-      propeller:require("./low/HAT/HAT_01 (Propeller hat)/HAT_01_Skin_01.png").default,
-      Trucker:require("./low/HAT/HAT_02 (Trucker hat)/HAT_02_Skin_01.png").default,
-      baseball:require("./low/HAT/HAT_03 (Baseball cap)/HAT_03_Skin_01.png").default,
-      Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_01.png").default,
-      Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_01.png").default,
-      Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_01.png").default,
-      none:""
-      
-    },
-    beard:{
-      regularMustache:{
-        black:require("./low/beard/FC01_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC01_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC01_Brown_Skin01.png").default,
-      },
-      curlyMustache:{
-        black:require("./low/beard/FC02_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC02_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC02_Brown_Skin01.png").default,
-      },
-      fullBeard:{
-        black:require("./low/beard/FC03_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC03_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC03_Brown_Skin01.png").default,
-      },     
-      shortBeard:{
-        black:require("./low/beard/FC04_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC04_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC04_Brown_Skin01.png").default,
-      },    
-      Goatee:{
-        black:require("./low/beard/FC05_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC05_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC05_Brown_Skin01.png").default,
-      }
-    }
-
-    // beard:{
-    //   ReqularMustache:{
-    //     black:require("./low/FACIAL HAIR/FC_01 (Regular Mustache)/FC_01_Black/FC01_Black_Skin01.png").default,
-    //     blonde:require("./low/FACIAL HAIR/FC_01 (Regular Mustache)/FC_01_Blonde/FC01_Blonde_Skin01.png").default,
-    //     brown:require("./low/FACIAL HAIR/FC_01 (Regular Mustache)/FC_01_Brown/FC01_Brown_Skin01.png").default,
-    //   },
-    //   Fullbeard:{
-    //     black:require("./low/FACIAL HAIR/FC_01 (Full beard)/FC_01_Black/FC01_Black_Skin01.png").default,
-    //     blonde:require("./low/FACIAL HAIR/FC_01 (Full beard)/FC_01_Blonde/FC01_Blonde_Skin01.png").default,
-    //     brown:require("./low/FACIAL HAIR/FC_01 (Full beard)/FC_01_Brown/FC01_Brown_Skin01.png").default,
-    //   },
-    // }
-  },
-  skin2:{
-    body:require("./low/BODY/Skin_02.png").default,
-    eye:require( "./low/EYES/EYES_01 (Regular oval)/EYES_01_Skin_02.png").default,
-    eyeBrow:require( "./low/EYEBROWS/EYEBROWS_01 (Slanted outward)/EYEBROWS_01_Skin_02.png").default,
-    ears:require( "./low/EARS/EARS_01 (Regular)/Ears01_Skin02.png").default,
-    mouth:require( "./low/MOUTH/MOUTH_02 (Open smile)/Mouth02_Skin02.png").default,
-    nose:require( "./low/NOSE/NOSE_01 (Circular big)/NOSE_01_Skin_02.png").default,
-    glasses:{
-      Regular:require("./low/GLASSES/GLASSES_01 (Regular glasses)/GLASSES_01_Skin_02.png").default,
-      Hippie:require("./low/GLASSES/GLASSES_03 (Hippie)/GLASSES_03_Skin_02.png").default,
-      Sunglasses:require("./low/GLASSES/GLASSES_04 (Sunglasses)/GLASSES_04_Skin_02.png").default,
-      JOY:require("./low/GLASSES/GLASSES_02 (JOY glasses)/GLASSES_02_Skin_02.png").default,
-      Round: require("./low/GLASSES/GLASSES_05 (Round metal frame glasses)/GLASSES_05_Skin_02.png").default,
-      patch:require("./low/GLASSES/GLASSES_06 (Eye patch)/GLASSES_06_Skin_02.png").default,
-      threeD:require("./low/GLASSES/GLASSES_07 (3D glasses)/GLASSES_07_Skin_02.png").default
-    }, hat:{
-      propeller:require("./low/HAT/HAT_01 (Propeller hat)/HAT_01_Skin_02.png").default,
-      Trucker:require("./low/HAT/HAT_02 (Trucker hat)/HAT_02_Skin_02.png").default,
-      baseball:require("./low/HAT/HAT_03 (Baseball cap)/HAT_03_Skin_02.png").default,
-      Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_02.png").default,
-      Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_02.png").default,
-      Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_02.png").default,
-    },
-    beard:{
-      regularMustache:{
-        black:require("./low/beard/FC01_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC01_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC01_Brown_Skin01.png").default,
-      },
-      curlyMustache:{
-        black:require("./low/beard/FC02_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC02_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC02_Brown_Skin01.png").default,
-      },
-      fullBeard:{
-        black:require("./low/beard/FC03_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC03_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC03_Brown_Skin01.png").default,
-      },     
-      shortBeard:{
-        black:require("./low/beard/FC04_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC04_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC04_Brown_Skin01.png").default,
-      },    
-      Goatee:{
-        black:require("./low/beard/FC05_Black_Skin01.png").default,
-        blonde:require("./low/beard/FC05_Blonde_Skin01.png").default,
-        brown:require("./low/beard/FC05_Brown_Skin01.png").default,
-      }
-    }
-  },
-  // skin3:{
-  //   body:require("./Avatar/BODY/Skin_03.png").default,
-  //   eye:require( "./Avatar/EYES/EYES_01 (Regular oval)/EYES_01_Skin_03.png").default,
-  //   eyeBrow:require( "./Avatar/EYEBROWS/EYEBROWS_01 (Slanted outward)/EYEBROWS_01_Skin_03.png").default,
-  //   ears:require( "./Avatar/EARS/EARS_01 (Regular)/Ears01_Skin03.png").default,
-  //   mouth:require( "./Avatar/MOUTH/MOUTH_02 (Open smile)/Mouth02_Skin03.png").default,
-  //   nose:require( "./Avatar/NOSE/NOSE_01 (Circular big)/NOSE_01_Skin_03.png").default,
-  //   glasses:{
-  //     Regular:require("./Avatar/GLASSES/GLASSES_01 (Regular glasses)/GLASSES_01_Skin_03.png").default,
-  //     Hippie:require("./Avatar/GLASSES/GLASSES_03 (Hippie)/GLASSES_03_Skin_03.png").default,
-  //     Sunglasses:require("./Avatar/GLASSES/GLASSES_04 (Sunglasses)/GLASSES_04_Skin_03.png").default,
-  //     JOY:require("./Avatar/GLASSES/GLASSES_02 (JOY glasses)/GLASSES_02_Skin_03.png").default,
-  //     Round: require("./Avatar/GLASSES/GLASSES_05 (Round metal frame glasses)/GLASSES_05_Skin_03.png").default,
-  //     patch:require("./Avatar/GLASSES/GLASSES_06 (Eye patch)/GLASSES_06_Skin_03.png").default,
-  //     threeD:require("./Avatar/GLASSES/GLASSES_07 (3D glasses)/GLASSES_07_Skin_03.png").default,
-  //     none:""
-  //   },hat:{
-  //     propeller:require("./low/HAT/HAT_01 (Propeller hat)/HAT_01_Skin_03.png").default,
-  //     Trucker:require("./low/HAT/HAT_02 (Trucker hat)/HAT_02_Skin_03.png").default,
-  //     baseball:require("./low/HAT/HAT_03 (Baseball cap)/HAT_03_Skin_03.png").default,
-  //     Beanie:require("./low/HAT/HAT_04 (Beanie)/HAT_04_Skin_03.png").default,
-  //     Bandana:require("./low/HAT/HAT_05 (Bandana)/HAT_05_Skin_03.png").default,
-  //     Turban:require("./low/HAT/HAT_06 (Turban)/HAT_06_Skin_03.png").default,
-  //   },
-  //   beard:{
-  //     regularMustache:{
-  //       black:require("./low/beard/FC01_Black_Skin01.png").default,
-  //       blonde:require("./low/beard/FC01_Blonde_Skin01.png").default,
-  //       brown:require("./low/beard/FC01_Brown_Skin01.png").default,
-  //     },
-  //     curlyMustache:{
-  //       black:require("./low/beard/FC02_Black_Skin01.png").default,
-  //       blonde:require("./low/beard/FC02_Blonde_Skin01.png").default,
-  //       brown:require("./low/beard/FC02_Brown_Skin01.png").default,
-  //     },
-  //     fullBeard:{
-  //       black:require("./low/beard/FC03_Black_Skin01.png").default,
-  //       blonde:require("./low/beard/FC03_Blonde_Skin01.png").default,
-  //       brown:require("./low/beard/FC03_Brown_Skin01.png").default,
-  //     },     
-  //     shortBeard:{
-  //       black:require("./low/beard/FC04_Black_Skin01.png").default,
-  //       blonde:require("./low/beard/FC04_Blonde_Skin01.png").default,
-  //       brown:require("./low/beard/FC04_Brown_Skin01.png").default,
-  //     },    
-  //     Goatee:{
-  //       black:require("./low/beard/FC05_Black_Skin01.png").default,
-  //       blonde:require("./low/beard/FC05_Blonde_Skin01.png").default,
-  //       brown:require("./low/beard/FC05_Brown_Skin01.png").default,
-  //     }
-  //   }
-
-  // },
-}
+import items from "./loaders"
 const undependentItems={
   clothes:{
     cloth1:require("./low/CLOTHES/Clothes_01.png").default,
@@ -228,11 +54,13 @@ const App = () => {
   const stageRef = React.useRef(null);
 const [skin,setSkin] = useState(items.skin1)
 const [_glasses,setGlasses] = useState(skin.glasses.Regular)
+const [_eyebrow,seteyebrow] = useState(skin.eyebrow.slanted)
+
 const [_hat,sethat] = useState(skin.hat.propeller)
 const [_beard,setbeard] = useState(skin.beard.fullBeard.brown)
 const [_clothes,setclothes] = useState(undependentItems.clothes.cloth1)
 // const [_,set] = useState(skin.glasses.Regular)
-const {ears,body,eye,eyeBrow,mouth,nose,glasses,hat,beard} =skin
+const {ears,body,eye,mouth,nose,glasses,hat,beard,eyebrow} =skin
 const {clothes,harHat}=undependentItems
 
    const handleExport = () => {
@@ -260,17 +88,21 @@ const {clothes,harHat}=undependentItems
   function changeBeard(breard,isColor){
     setbeard(breard.black)
   }
-
-  console.log("gogogogo",_clothes)
+function changeeyebrow(eyebrow){
+  seteyebrow(eyebrow)
+}
+ 
+const controllers=[
+  [glasses,changeGlasses],
+  [clothes,changeCloth],
+  [hat,changeHat],
+  [beard,changeBeard],
+  [eyebrow,changeeyebrow],
+]
 
     return (
       <div>
-        <UI handleExport={handleExport} changeGlasses={changeGlasses} 
-        glasses={glasses} changeSkin={changeSkin} skins={items} 
-        clothes={clothes} changeCloth={changeCloth}
-      hats={hat} changeHat={changeHat}
-      beard={beard} changeBeard={changeBeard}
-        /> 
+        <UI handleExport={handleExport} controllers={controllers} /> 
        
       <Stage width={1000} height={1000} ref={stageRef}>
         <Layer>
@@ -281,7 +113,7 @@ const {clothes,harHat}=undependentItems
         <LionImage imageSource ={harHat.black} y={-20} />
           <LionImage imageSource ={body}  />
           <LionImage imageSource ={eye}  />
-          <LionImage imageSource ={eyeBrow}  />
+          <LionImage imageSource ={_eyebrow}  />
           <LionImage imageSource ={mouth}  />
           <LionImage imageSource ={_glasses}  />
           <LionImage imageSource ={_hat}  />
