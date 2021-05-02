22709feb8b874a1fffeb967d415d40a45ee4cac4
= <hodaman2012@gmail.com>
2021-05-02 23:51:26 +0200

    linear gradient

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import reportWebVitals from './reportWebVitals';
 import "./index.css"
 import React, {useState } from 'react';
 import { render } from 'react-dom';
-import { Stage, Layer, Image } from 'react-konva';
+import { Stage, Layer, Image ,Rect} from 'react-konva';
 import useImage from 'use-image';
 import UI from "./UI"
 import items from "./loaders"
@@ -44,6 +44,7 @@ const beardColors = {
   blonde:"blonde",
   brown:"brown",
 }
+
 const [_glasses,setGlasses] = useState(glasses.Regular)
 const [_eyebrow,seteyebrow] = useState(eyebrow.slanted)
 const [_hat,sethat] = useState(hat.propeller)
@@ -54,23 +55,21 @@ const [_clothes,setclothes] = useState(undependentItems.clothes.cloth1)
 const [_hair,sethair]=useState(hair['Buzz cut'])
 const {clothes,harHat}=undependentItems
 
-
-  
-  function changeSkin(skintone){
-    setSkin(skintone)
-  }
-  function changeGlasses(glass){
-    setGlasses(glass)
-  }
-  function changeCloth(cloth){
-    setclothes(cloth)
-  }
-  function changeHat(hat){
-    sethat(hat)
-  }
-  function changeBeard(breard,isColor){
-    setbeard(breard)
-  }
+function changeSkin(skintone){
+  setSkin(skintone)
+}
+function changeGlasses(glass){
+  setGlasses(glass)
+}
+function changeCloth(cloth){
+  setclothes(cloth)
+}
+function changeHat(hat){
+  sethat(hat)
+}
+function changeBeard(breard,isColor){
+  setbeard(breard)
+}
 function changeeyebrow(eyebrow){
   seteyebrow(eyebrow)
 }
@@ -79,10 +78,7 @@ function changeeye(e){
 }
 
 function changebeardColor(col){
-  // const oldBeard = _beard
-  // console.log("nonono",oldBeard)
-  // setbeard(oldBeard[col])
-setBeardColor(col)
+  setBeardColor(col)
 }
 
 const controllers=[
@@ -102,16 +98,26 @@ const controllers=[
        
       <Stage width={1000} height={1000} ref={stageRef}>
         <Layer>
+        <Rect
+          x={0}
+          y={0}
+          width={1000}
+          height={1000}
+          fillLinearGradientEndPointX={1000}
+          fillLinearGradientStartPointX={0}
+          fillLinearGradientStartPointY={0}
+          fillLinearGradientEndPointY={1000}
+          fillLinearGradientColorStops={[0,"#f0f",1,"#ff0"]}
+        />
           <LionImage imageSource ={ears}  />
           <LionImage imageSource ={body}  />
-        <LionImage imageSource ={_clothes}  />
+         <LionImage imageSource ={_clothes}  />
           <LionImage imageSource ={mouth}  />
+          <LionImage imageSource ={_hair[beardColor]}  />
+
           <LionImage imageSource ={_beard[beardColor]}  />
           {/* <LionImage imageSource ={_hat}  /> */}
         {/* <LionImage imageSource ={harHat.black} y={-20} /> */}
-
-          <LionImage imageSource ={_hair[beardColor]}  />
-          
           <LionImage imageSource ={_eye}  />
           <LionImage imageSource ={_eyebrow}  />
           <LionImage imageSource ={_glasses}  />
@@ -120,7 +126,7 @@ const controllers=[
         {/* <LionImage imageSource ={harHat.black} x={20} y={40} /> */}
 
           {/* <LionImage imageSource ={_clothes}  /> */}
-
+        
         </Layer>
       </Stage>
       </div>
